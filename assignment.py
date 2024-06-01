876f98f8ca24c965de33525fa30578fdc2467fae
olawa001 <abdullateefyusuf80@gmail.com>
2024-06-01 18:30:22 +0100

    first commit

--- assignment.py
+++ assignment.py
@@ -1,18 +1,18 @@
-# #1 maximum of three number
-# def find_maximum(num1, num2, num3):
-#     if num1 >= num2 and num1 >= num3:
-#         return num1
-#     elif num2 >= num1 and num2 >= num3:
-#         return num2
-#     else:
-#         return num3
+#1 maximum of three number
+def find_maximum(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
 
-# number1 = float(input("Enter the first number: "))
-# number2 = float(input("Enter the second number: "))
-# number3 = float(input("Enter the third number: "))
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
 
-# maximum = find_maximum(number1, number2, number3)
-# print("Maximum number is:", maximum)
+maximum = find_maximum(number1, number2, number3)
+print("Maximum number is:", maximum)
 
 
 
